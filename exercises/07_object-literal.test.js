e09016957c3ac05121eda3fc9b94c15b8210af0b
Juan Narowé <juannarowe@gmail.com>
2026-02-05 18:33:44 +0100

    feat: resolve object literal shorthand and computed properties

--- exercises/07_object-literal.test.js
+++ exercises/07_object-literal.test.js
@@ -1,5 +1,13 @@
 test('07_object-literal-1: can use shorthand for property names', () => {
   const  createMonster = (name, power) => {
+    return {
+      type: 'Monster',
+      name,
+      power,
+      attack(target){
+        return `${this.name} attacked ${target.name}`;
+      }
+    }
     // Utilitza la nova sintaxi del literal d'Object, per tornar un literal que permeti passar els testos
     // return {
     //   type: 'Monster',
@@ -23,6 +31,7 @@ test('07_object-literal-2: can use expressions as property names', () => {
     return {
       tasty: true,
       type,
+      [type.toUpperCase() + type.length]: description
       // afegeix una expressió com a nom de propietat on el nom de la propietat sigui el resultat de type.toUpperCase() + type.length
       // sembla forçat? Ho és... 😅
     }
