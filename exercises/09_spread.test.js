2953383a171e313b7ea1a1caff4e662f1f8faa66
juannarowe <juannarowe@gmail.com>
2026-02-08 20:33:13 +0100

    feat: resolve spread operator exercises for arrays and objects

--- exercises/09_spread.test.js
+++ exercises/09_spread.test.js
@@ -10,14 +10,19 @@ test('09_spread-1: should be able to call a function and spread the arguments', 
     calls++
   }
   
+  // SOLUCIÓN: "Esparcimos" el array dentro de la llamada a la función
+  myFunction(...args)
+  
   expect(calls).toBe(1)
-
 })
 
 test('09_spread-2: should be easier to concatenate arrays', () => {
   // Utilitza l'operador spread per concatenar arrays
   const array1 = [1, 2, 3]
+  const array2 = [4, 5, 6] // Definimos el segundo array
   
+  // SOLUCIÓN: Creamos un nuevo array con los elementos de ambos
+  const result = [...array1, ...array2]
   
   expect(result).toEqual([1, 2, 3, 4, 5, 6])
 })
@@ -29,6 +34,8 @@ test('09_spread-3: should be able to merge properties from objects', () => {
     baz: 'foobar',
   }
 
+  // SOLUCIÓN: Clonamos obj1 y añadimos la nueva propiedad
+  const result = { ...obj1, eggs: 'spam' }
 
   expect(result).toEqual({
     foo: 'bar',
